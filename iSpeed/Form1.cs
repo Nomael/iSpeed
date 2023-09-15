c915c027a70c9cb31bddb7d3c8bb2312b66c1d1c
xlion007 <elias040704@gmail.com>
2023-09-15 09:34:19 +0200

    Klasse versendung erstellt

--- iSpeed/Form1.cs
+++ iSpeed/Form1.cs
@@ -16,5 +16,10 @@ namespace iSpeed
         {
             InitializeComponent();
         }
+
+        private void btn_Portorechnen_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
